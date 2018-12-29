8cbef4137a215b58545d0426e459c40d8e7b8a4b
mohammadkhadarbasha <bashacherry197@gmail.com>
2018-12-29 17:10:56 +0530

    BaseClass

---
+++ src/org/baseclass/Baseclasss.java
@@ -0,0 +1,14 @@
+package org.baseclass;
+
+public class Baseclasss {
+	public static void main(String[] args) {
+		
+	}
+	private void employid() {
+		System.out.println("employid");
+	}
+	private void empname() {
+		System.out.println("emp name");
+	}
+
+}
